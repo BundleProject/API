63cf62bd08ff5b00f5671817913fc4e30117cb5e
TymanWasTaken <tyman@tyman.tech>
2021-08-11 17:16:03 -0600

    use env vars not properties for port

--- src/main/kotlin/org/bundleproject/Application.kt
+++ src/main/kotlin/org/bundleproject/Application.kt
@@ -7,14 +7,7 @@ import org.bundleproject.plugins.*
 import org.bundleproject.utils.fetchAssets
 
 fun main() {
-    val portProp = System.getProperty("server.port")
-    var port = 8080
-    if (portProp != null) {
-        try {
-            port = portProp.toInt()
-        } catch (ignored: NumberFormatException) {}
-    }
-    embeddedServer(Netty, port = port, host = "0.0.0.0") {
+    embeddedServer(Netty, port = System.getenv("PORT")?.toIntOrNull() ?: 8080, host = "0.0.0.0") {
         configureRouting()
         configureSerialization()
     }.start(wait = true)
