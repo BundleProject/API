0c4db3bc84b0e540baafef38cb6b4fa498f8f1d3
TymanWasTaken <tyman@tyman.tech>
2021-08-11 20:41:55 -0600

    Add 2nd endpoint and start on tests

--- src/main/kotlin/org/bundleproject/plugins/Routing.kt
+++ src/main/kotlin/org/bundleproject/plugins/Routing.kt
@@ -60,5 +60,42 @@ fun Application.configureRouting() {
                 )
             }
         }
+        get("/v1/mods/{id}/{platform}/{minecraftVersion}/{version}/download") {
+            val assets = fetchAssets()
+            val id = call.parameters["id"]!!
+            val platform = call.parameters["platform"]!!
+            val minecraftVersion = call.parameters["minecraftVersion"]!!
+            val version = call.parameters["version"]!!
+            val mod = assets.mods[id]
+            val modData = mod
+                ?.platforms
+                ?.get(platform)
+                ?.get(minecraftVersion)
+                ?.get(version)
+                ?: return@get call.respond(
+                    HttpStatusCode.NotFound,
+                    mapOf(
+                        "success" to false,
+                        "error" to "Invalid mod"
+                    )
+                )
+            try {
+                call.respondRedirect(resolveUrl(ModData(
+                    version = version,
+                    source = modData.source,
+                    ref = modData.ref,
+                    name = id,
+                    id = modData.id
+                )), permanent = true)
+            } catch (ignored: ModNotFoundException) {
+                call.respond(
+                    HttpStatusCode.InternalServerError,
+                    mapOf(
+                        "success" to false,
+                        "error" to "Failed to fetch download url from source"
+                    )
+                )
+            }
+        }
     }
 }
--- src/test/kotlin/org/bundleproject/ApplicationTest.kt
+++ src/test/kotlin/org/bundleproject/ApplicationTest.kt
@@ -2,15 +2,23 @@ package org.bundleproject
 
 import io.ktor.http.*
 import io.ktor.server.testing.*
+import kotlinx.coroutines.runBlocking
+import org.bundleproject.json.assets.ModAssets
 import org.bundleproject.plugins.configureRouting
+import org.bundleproject.utils.fetchAssets
 import kotlin.test.Test
 import kotlin.test.assertEquals
 
 class ApplicationTest {
     @Test
-    fun testRoot() {
+    fun testMod() {
         withTestApplication({ configureRouting() }) {
-            handleRequest(HttpMethod.Get, "/").apply {
+            var assets: ModAssets
+            runBlocking {
+                assets = fetchAssets()
+            }
+            val asset = assets.mods[assets.mods.keys.first()]!!
+            handleRequest(HttpMethod.Get, "/v1/mods/${asset.platforms.keys.first()}").apply {
                 assertEquals(HttpStatusCode.OK, response.status())
                 assertEquals("Hello World!", response.content)
             }
