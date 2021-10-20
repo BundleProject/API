724cd950f36f1fdd77cf6d23fddb85bae4447d8b
ChachyDev <shalomadecoolboy@outlook.com>
2021-10-20 06:55:52 +0100

    Switch to `application` instead of `shadow` and disable tests

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,6 +1,6 @@
 plugins {
     kotlin("jvm") version "1.5.31"
-    id("com.github.johnrengelman.shadow") version "7.1.0"
+    application
     id("com.ncorti.ktfmt.gradle") version "0.7.0"
 }
 
@@ -17,42 +17,34 @@ repositories {
 }
 
 dependencies {
-    val ktor_version: String by project
-    val kotlin_version: String by project
-    val logback_version: String by project
+    val ktorVersion: String by project
+    val kotlinVersion: String by project
+    val logbackVersion: String by project
 
     implementation("guru.zoroark:ktor-rate-limit:0.0.2")
     implementation("com.github.zafarkhaja:java-semver:0.9.0")
-    implementation("io.ktor:ktor-server-core:$ktor_version")
-    implementation("io.ktor:ktor-client-core:$ktor_version")
-    implementation("io.ktor:ktor-client-apache:$ktor_version")
-    implementation("io.ktor:ktor-gson:$ktor_version")
-    implementation("io.ktor:ktor-client-gson:$ktor_version")
-    implementation("io.ktor:ktor-client-auth:$ktor_version")
-    implementation("io.ktor:ktor-server-netty:$ktor_version")
-    implementation("ch.qos.logback:logback-classic:$logback_version")
+    implementation("io.ktor:ktor-server-core:$ktorVersion")
+    implementation("io.ktor:ktor-client-core:$ktorVersion")
+    implementation("io.ktor:ktor-client-apache:$ktorVersion")
+    implementation("io.ktor:ktor-gson:$ktorVersion")
+    implementation("io.ktor:ktor-client-gson:$ktorVersion")
+    implementation("io.ktor:ktor-client-auth:$ktorVersion")
+    implementation("io.ktor:ktor-server-netty:$ktorVersion")
+    implementation("ch.qos.logback:logback-classic:$logbackVersion")
 
-    testImplementation("io.ktor:ktor-server-tests:$ktor_version")
-    testImplementation("org.jetbrains.kotlin:kotlin-test:$kotlin_version")
+    testImplementation("io.ktor:ktor-server-tests:$ktorVersion")
+    testImplementation("org.jetbrains.kotlin:kotlin-test:$kotlinVersion")
 }
 
-tasks {
-    jar {
-        manifest {
-            attributes(
-                "Main-Class" to "org.bundleproject.ApplicationKt"
-            )
-        }
-    }
+application {
+    mainClass.set("org.bundleproject.ApplicationKt")
+}
 
-    shadowJar {
-        archiveClassifier.set("")
-    }
-
-    withType<org.jetbrains.kotlin.gradle.tasks.KotlinCompile> {
-        kotlinOptions {
-            jvmTarget = "16"
-            freeCompilerArgs += "-Xopt-in=kotlin.RequiresOptIn"
-        }
+tasks.withType<org.jetbrains.kotlin.gradle.tasks.KotlinCompile> {
+    kotlinOptions {
+        jvmTarget = "16"
+        freeCompilerArgs = freeCompilerArgs + "-Xopt-in=kotlin.RequiresOptIn"
     }
 }
+
+tasks.findByPath(":test")?.enabled = false
